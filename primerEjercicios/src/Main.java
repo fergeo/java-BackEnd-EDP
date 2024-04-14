fc627f9d682b77c8c8b5be7333d368f5dd6d77c5
fergeo <fergeo@yahoo.com>
2024-04-14 16:54:36 -0300

    Actualizaciones
    Se agrego las funciones de calculo de IMX, conversion de pesaos a dolares.
    Manejo de setters, setters y clases.

--- primerEjercicios/src/Main.java
+++ primerEjercicios/src/Main.java
@@ -1,6 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.SQLOutput;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Main {
 
@@ -83,14 +85,57 @@ public class Main {
 
     }
 
+//    Ejercicio 1: Calculadora de IMC (Índice de Masa Corporal)
+//    Crea un programa que solicite al usuario ingresar su peso (en kilogramos) y su altura (en metros). L
+//    uego, calcula el IMC (peso / altura^2) y muestra un mensaje indicando si la persona tiene un peso bajo,
+//    normal, sobrepeso o obesidad, según los valores típicos del IMC.
+    public static void calculador(){
+
+        float peso, altura;
+        double IMC;
+
+        Scanner lectura = new Scanner(System.in);
+
+        System.out.println("Ingrese su peso en Kilogramos: ");
+        peso = lectura.nextFloat();
+
+        System.out.println("Ingrese su altura en metros ");
+        altura = lectura.nextFloat();
+
+        IMC = ( peso / Math.pow( altura , 2 ));
+
+        DecimalFormat df = new DecimalFormat("#,##0.00");
+        System.out.println("El IMC es " + df.format(IMC));
+
+    }
+
+ //     Ejercicio 2: Conversor de Monedas
+ //     Desarrolla un programa que permita al usuario convertir una cantidad de pesos argentinos a dolares.
+ //     El usuario debe ingresar la cantidad en pesos, y el programa debe realizar la conversión y mostrar el resultado.
+ //     Utiliza una tasa de conversión fija.
+    public static void conversor(){
+
+        final double TAZA = 1000.51;
+
+        float pesos;
+        Scanner lectura = new Scanner(System.in);
+
+        System.out.println("Engrese la cantidad en pesaso argnetinos: ");
+        pesos = lectura.nextFloat();
+
+        DecimalFormat df = new DecimalFormat("#,##0.00");
+        System.out.println("El valor ingresado en dolares es " + df.format(pesos * TAZA));
+    }
 
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        ejercicio1();
-        ejercicio2();
-        ejercicio3();
-        ejercicio4();
+        //ejercicio1();
+        //ejercicio2();
+        //ejercicio3();
+        //ejercicio4();
+        //calculador();
+        conversor();
 
     }
 }
